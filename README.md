1f54467772564bffffeb5f8b6f4a8895bdcf131d
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-09-11 01:00:18 -0400

    Start

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 This is a single page application that lets users register and manage their offchain ENS subnames via the NameStone API.
 
+## Tools
+
+### NameStone
+
+Namestone is an API for easy subdomain management and issuance. Follow [their docs](https://namestone.xyz/docs) to set up a free account.
+
+### Pinata
+
+Pinata is a cloud storage provider that supports pinning content to IPFS. We use it to store ENS avatar images. Follow [this guide](https://docs.pinata.cloud/frameworks/next-js#setup-pinata-2) to create the relevant API key (it needs Admin permissions).
+
 ## Configuration
 
 - Change the `parentDomain` in [`src/lib/namestone.ts`](./src/lib/namestone.ts#L5) to your desired domain.
--- src/components/NameManager.tsx
+++ src/components/NameManager.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useSIWE } from 'connectkit'
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useFormState, useFormStatus } from 'react-dom'
 import { useAccount } from 'wagmi'
 
@@ -12,6 +12,7 @@ import { Input } from '@/components/Input'
 import { useIsMounted } from '@/hooks/useIsMounted'
 import { useNamestone } from '@/hooks/useNamestone'
 import { parentDomain } from '@/lib/namestone'
+import { pinata } from '@/lib/pinata'
 import { cn } from '@/lib/utils'
 
 import { ConnectButton } from './ConnectButton'
@@ -26,6 +27,7 @@ export function NameManager({
   const names = useNamestone(address)
   const isMounted = useIsMounted()
 
+  const [imgUploading, setImgUploading] = useState(false)
   const [createState, createAction] = useFormState(createName, {})
   const [updateState, updateAction] = useFormState(updateName, {})
 
@@ -46,6 +48,10 @@ export function NameManager({
         className={cn('flex w-full max-w-md flex-col gap-2', className)}
         action={updateAction}
       >
+        <FileUploader
+          imgUploading={imgUploading}
+          setImgUploading={setImgUploading}
+        />
         <input type="hidden" name="name" value={names.data.first.name} />
         <input type="hidden" name="address" value={address} />
         <input type="hidden" name="domain" value={parentDomain} />
@@ -64,13 +70,6 @@ export function NameManager({
             defaultValue={names.data.first.text_records?.['org.telegram']}
           />
         </div>
-
-        {/* <Input
-          name="description"
-          label="Description"
-          placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia."
-          defaultValue={names.data.first.text_records?.description}
-        /> */}
 
         <SubmitButton text="Update" className="mt-1 sm:mt-0" />
 
@@ -128,19 +127,67 @@ export function NameManager({
 function SubmitButton({
   text,
   className,
+  imgUploading,
 }: {
   text: string
   className?: string
+  imgUploading?: boolean
 }) {
   const { pending } = useFormStatus()
 
   return (
     <Button
       type="submit"
-      loading={pending}
+      loading={pending || imgUploading}
       className={cn('rounded-lg', className)}
     >
       {text}
     </Button>
+  )
+}
+
+function FileUploader({
+  imgUploading,
+  setImgUploading,
+}: {
+  imgUploading: boolean
+  setImgUploading: (uploading: boolean) => void
+}) {
+  const [ipfsUri, setIpfsUri] = useState('')
+
+  const uploadFile = async (file: File) => {
+    try {
+      setImgUploading(true)
+      const keyRequest = await fetch('/api/pinata/key')
+      const keyData = await keyRequest.json()
+      const upload = await pinata.upload.file(file).key(keyData.JWT)
+      setImgUploading(false)
+      console.log(upload)
+      setIpfsUri(upload.cid)
+    } catch (e) {
+      console.error(e)
+      setImgUploading(false)
+      alert('Trouble uploading file')
+    }
+  }
+
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+
+    if (!file) {
+      alert('No file selected')
+      return
+    }
+
+    await uploadFile(file)
+  }
+
+  return (
+    <>
+      <input type="file" onChange={handleFileChange} />
+      {ipfsUri && (
+        <input type="hidden" name="avatar" value={`ipfs://${ipfsUri}`} />
+      )}
+    </>
   )
 }
